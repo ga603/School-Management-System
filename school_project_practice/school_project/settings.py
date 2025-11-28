0bbbf2ea4dcf094a5fdbb9c4f176f294a6df863e
John Gachie <gachiej585@gmail.com>
2025-11-28 19:13:39 +0300

    Fixed Allowed Hosts

--- school_project_practice/school_project/settings.py
+++ school_project_practice/school_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-^21%-2)a18)#g7no77_(2v7kz!z%^6yam$)2em@lf)09htnrnj
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['kinariportal.pythonanywhere.com']
 
 
 # Application definition
